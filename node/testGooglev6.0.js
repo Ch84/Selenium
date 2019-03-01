2845644b7a6c10ba274a04473c826c57e811204e
Ch84 <33604841+Ch84@users.noreply.github.com>
2019-03-01 22:02:00 +0100

    Tenth commit. Just continued working on testGoogle js file version 6.0. Modified it by moving the code within the Time handler instead to make webdriver tests more stable.

---
+++ node/testGooglev6.0.js
@@ -0,0 +1,97 @@
+// Call webdriver from lib
+
+var webdriver = require('selenium-webdriver');
+
+// Set up tools
+
+By = webdriver.By,
+until = webdriver.until;
+
+// Set up browser
+
+var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+
+// Get web page
+
+driver.get('https://www.google.se');
+
+// Execute time handler tool
+
+const fs = require('fs');
+const EventEmitter = require('events');
+
+class WithTime extends EventEmitter {
+
+    execute(asyncFunc, ...args) {
+
+        this.emit('begin');
+        console.time('execute');
+        asyncFunc(...args, (err, data) => {
+
+            if (err) {
+
+                return this.emit('error', err);
+
+            }
+
+            this.emit('data', data);
+            console.timeEnd('execute');
+            this.emit('end');
+
+        });
+
+    }
+
+}
+
+const withTime = new WithTime();
+
+withTime.on('begin', () => console.log('About to execute'));
+
+// Maximize browser window
+
+driver.manage().window().maximize();
+
+// Execute tests
+
+var searchField = driver.findElement(By.name('q'));
+searchField.sendKeys('webdriver');
+
+var clickSearchBtn = driver.findElement(By.name('btnK'));
+clickSearchBtn.click();
+
+// Driver will sleep here
+
+driver.sleep(20000);
+
+var clickToolsBtn = driver.wait(until.elementLocated(By.xpath("//a[@id='hdtb-tls']")));
+clickToolsBtn.isEnabled().then(function() {
+
+    // Driver will pause here
+
+    driver.sleep(10000);
+    clickToolsBtn.click();
+
+        // Create new variables within function
+
+        var chooseCountry = driver.wait(until.elementLocated(By.css('div.mn-hd-txt')));
+        chooseCountry.isDisplayed().then(function() {
+
+            chooseCountry.click();
+
+        });
+
+        var chooseCountry2 = driver.wait(until.elementLocated(By.xpath("//li[@id='ctr_countrySE']")));
+        chooseCountry2.isDisplayed().then(function() {
+
+            chooseCountry2.click();
+
+        });
+
+        console.log('Finished exeuting tests');
+
+});
+
+withTime.on('end', () => console.log('Done with execute'));
+
+withTime.execute(fs.readFile, __filename);
