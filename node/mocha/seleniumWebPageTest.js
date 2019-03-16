3f93d436cbb872a8d6d081ebdf251b664bd687b1
Ch84 <33604841+Ch84@users.noreply.github.com>
2019-03-16 01:42:28 +0100

    Continued build on seleniumWebPageTest.js. Solved how to click element.

---
+++ node/mocha/seleniumWebPageTest.js
@@ -0,0 +1,68 @@
+// Call webdriver and set up debugger
+
+const debug = require('debug')('swdtest');
+const {By, Key, until} = require('selenium-webdriver');
+const {suite} = require('selenium-webdriver/testing');
+
+// Set up Selenium Webdriver Suite
+
+suite(function(env) {
+
+    // Mocha BDD and set up describe scenario
+
+    describe('Selenium Webdriver Test Page Scenario', async function() {
+
+        let driver;
+
+        // Set up the debugger for driver before execution of tests
+
+        before(async function() {
+
+            debug('driver setup');
+            driver = await env.builder().build();
+
+        });
+
+        // Close the browser after execution is complete
+
+        after(function() {
+
+            debug('driver teardown');
+            driver.quit();
+            driver = null;
+
+        });
+
+        // Execute tests
+
+        it('should maximize the browser window', async function() {
+            
+            debug('maximizing window');
+            await driver.manage().window().maximize();
+            debug('done');
+
+        });
+
+        it('it should fetch the test site', async function() {
+
+            debug('fetching site');
+            driver.get('https://www.seleniumhq.org');
+            debug('done');
+
+        });
+
+        it('should find element', async function() {
+
+            debug('find element');
+            await driver.sleep(5000);
+            let el = await driver.findElement(By.xpath("//a[@title='Technical references and guides']"));
+            debug('click element');
+            await el.sendKeys(Key.TAB);
+            await el.sendKeys(Key.RETURN);
+            debug('done');
+
+        });
+
+    });
+
+});
