6b59520909d8c68575bf13f88f218b2421c3a29e
Ch84 <33604841+Ch84@users.noreply.github.com>
2019-03-24 21:20:09 +0100

    Continued build of async automation tests against Google, all tests successful so far. Also set down the driver.sleep to 2000 ms and removed the extra line driver.sleep with 5000 ms because of unnessecary execute.

--- node/mocha/googleTestv2.0.js
+++ node/mocha/googleTestv2.0.js
@@ -32,6 +32,79 @@ suite(function(env) {
 
         });
 
+        // Execute async automation tests
+
+        it('should maximize the browser window', async function() {
+
+            debug('maximized the browser window');
+            await driver.manage().window().maximize();
+            debug('done');
+
+        });
+
+        it('should fetch the Google Web Page', async function() {
+
+            debug('fetching Google web page');
+            await driver.get('https://www.google.se');
+            debug('done');
+
+        });
+
+        it('should input text in the text search field', async function() {
+
+            debug('searching for webelement for text search field');
+            let inputTxtSearchField = await driver.findElement(By.name('q'));
+            debug('sent keys');
+            inputTxtSearchField.sendKeys('webdriver');
+            await driver.sleep(2000);
+            debug('done');
+
+        });
+
+        it('should click the search button', async function() {
+
+            debug('searching for webelement for search btn');
+            let clickSearchBtn = driver.findElement(By.name('btnK'));
+            debug('clicked search btn');
+            await (await clickSearchBtn.click());
+            await driver.sleep(2000);
+            debug('done');
+
+        });
+
+        it('should now click on the tools button', async function() {
+
+            debug('searching for webelement for tools btn');
+            let clickSearchLink = await driver.findElement(By.xpath("//a[@id='hdtb-tls']"));
+            debug('clicked the tools btn');
+            await (await clickSearchLink.click());
+            await driver.sleep(2000);
+            debug('done');
+
+        });
+
+        it('should click on the down arrow for choosing countries', async function() {
+
+            debug('searching for webelement for down arrow btn');
+            let clickDownArrowBtn = await driver.findElement(By.xpath("//div[@id='hdtbMenus']//div[2]//span[1]"));
+            debug('clicked down arrow btn');
+            await (await clickDownArrowBtn.click());
+            await driver.sleep(2000);
+            debug('done');
+
+        });
+
+        it('should click on a country', async function() {
+
+            debug('searching for webelement for country select');
+            let clickCountryBtn = await driver.findElement(By.xpath("//li[@id='ctr_countrySE']"));
+            debug('clicked the country btn');
+            await (await clickCountryBtn.click());
+            await driver.sleep(2000);
+            debug('done');
+
+        });
+
     });
 
 });
